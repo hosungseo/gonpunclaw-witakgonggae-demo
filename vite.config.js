1151b9bbc3e7db8676c82e8ab620fe34deab4d87
hosungseo <hosung.seo2026@gmail.com>
2026-04-17 11:11:52 +0900

    chore: derive pages base from repository name

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'gonpunclaw-witakgonggae'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/gonpunclaw-witakgonggae/',
+  base: `/${repoName}/`,
 })
